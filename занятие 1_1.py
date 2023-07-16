5ea4e067e253c8b0abd16c8ae0a2cb9c4342a595
MSI <nikulenkov09@gmail.com>
2023-07-16 16:20:59 +0300

    initial

---
+++ занятие 1_1.py
@@ -0,0 +1,38 @@
+# N ** 2
+def strcounter(s):
+    for sym in s:
+        counter = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                counter += 1
+        
+        print(sym, counter)
+
+strcounter('print')
+
+print('')
+
+# M * N
+def strcounter_2(s):
+    for sym in set(s):
+        counter = 0
+        for sub_sym in s:
+            if sym == sub_sym:
+                counter += 1
+
+        print(sym, counter)
+
+strcounter_2('printttt')
+
+print('')
+
+# N
+def strcounter_3(s):
+    syms_counter = {}
+    for sym in s:
+        syms_counter[sym] = syms_counter.get(sym, 0) + 1
+
+    for sym, count in syms_counter.items():
+        print(sym, count)
+
+strcounter_3('printttt')
